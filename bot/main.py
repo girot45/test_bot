66be16ba108f9f7ef31acb34e4db3b61f7f8b2a2
girot45 <98813117+girot45@users.noreply.github.com>
2024-03-12 21:26:54 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,14 +2,17 @@ import asyncio
 
 from bot.managers.bot_manager import dp, bot
 from bot.managers.log_manager import logger
+from bot.managers.scheduler_manager import scheduler
+from bot.src.handlers.callback import router_callback
 from bot.src.handlers.commands import router_commands
 from bot.src.handlers.text_answers import router_text
 
 
-dp.include_routers(router_commands, router_text)
+dp.include_routers(router_commands, router_text, router_callback)
 
 
 async def main():
+    scheduler.start()
     await dp.start_polling(bot)
 
 
