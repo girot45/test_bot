aaa37e83ec14333ae390f03f7847867df19c009d
girot45 <98813117+girot45@users.noreply.github.com>
2024-03-12 21:26:41 +0300

    Update scheduler_manager.py

--- bot/managers/scheduler_manager.py
+++ bot/managers/scheduler_manager.py
@@ -1,7 +1,7 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 from bot.managers.bot_manager import bot
-from bot.src.scripts.wb import get_card_info
+from bot.src.scripts.send_card_by_notif import sub_card_number
 
 scheduler = AsyncIOScheduler(timezone="Europe/Moscow")
 job_registry = {}
@@ -11,19 +11,19 @@ def turn_on_schedule_for_user(
         card_id
 ):
     global job_registry
-    if job_registry.get(tgid) is not None:
-        job_registry[tgid].append(card_id)
-    else:
-        job_registry[tgid] = [card_id]
-    scheduler.add_job(
-        get_card_info,
-        'cron',
-        id=f"{tgid}_{card_id}fivemin",
-        day_of_week='mon-sun',
-        seconds=300,
-        kwargs={'message': bot, 'chat_id': tgid, 'istomorrow': False},
-        max_instances=5)
 
+    if job_registry.get(tgid) is not None:
+        job_registry[tgid].add(card_id)
+    else:
+        job_registry[tgid] = {card_id}
+    if scheduler.get_job(job_id=f"{tgid}_{card_id}fivemin") is None:
+        scheduler.add_job(
+            sub_card_number,
+            'interval',
+            id=f"{tgid}_{card_id}fivemin",
+            seconds=15,
+            kwargs={'bot': bot, 'chat_id': tgid, 'item_id': card_id},
+            max_instances=5)
 
 
 def turn_off_schedule_for_user(tgid: int):
